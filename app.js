7b410bbce1e5f599385428b78d6d7a5f2c617c12
NazarBaloch04 <153612842+NazarBaloch04@users.noreply.github.com>
2024-04-18 01:00:41 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+function UserInfo(name, age, email, qualification) {
+    console.log("My name is ".concat(name, ", \nmy age is ").concat(age, ", \nmy email is ").concat(email, " \nand i am ").concat(qualification));
+}
+UserInfo("Nazar Mazari", 19, "mazarinazar@gmail.com", "underGraguated");
+UserInfo("Shoiaib", 23, "Shoaib@gmail.com", "Graguated");
---
+++ app.ts
@@ -0,0 +1,12 @@
+
+function UserInfo(
+  name: string,
+  age: number,
+  email: string,
+ qualification: string,
+
+) {
+  console.log(`My name is ${name}, \nmy age is ${age}, \nmy email is ${email} \nand i am ${qualification}`);
+}
+UserInfo("Nazar Mazari", 19 , "mazarinazar@gmail.com", "underGraguated")
+UserInfo("Shoiaib", 23 , "Shoaib@gmail.com", "Graguated")
